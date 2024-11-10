e9d80d90af259ac29b6ceb832551f9023622c3b4
Vikram.Reddy <pagadala1021@gmail.com>
2024-11-10 14:36:25 +0530

    sql leetcode  problem and solution

--- Leetcode/1285. Find the Start and End Number of Continuous Ranges.sql
+++ Leetcode/1285. Find the Start and End Number of Continuous Ranges.sql
@@ -55,3 +55,10 @@ Number 10 is contained in the table.
 --Solutions
 
 
+SELECT *,
+       (SELECT COUNT(*)
+        FROM Examinations e
+        WHERE e.student_id = s.student_id 
+        AND e.subject_name = s.subject_name) AS attended_exams
+FROM (SELECT * FROM Students CROSS JOIN Subjects) AS s
+ORDER BY s.student_id, s.subject_name;
